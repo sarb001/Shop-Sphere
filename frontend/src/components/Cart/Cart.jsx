0cdea711fb27570919834b9ed0be5e88a916e96c
SARB <mrsinghbusiness05@gmail.com>
2024-04-21 07:25:52 +0530

    cartui changed -frontend

--- frontend/src/components/Cart/Cart.jsx
+++ frontend/src/components/Cart/Cart.jsx
@@ -28,32 +28,44 @@ const Cart = () => {
 
         {cartitems?.length > 0 ? 
 
-        <div style = {{display:'grid' , gridTemplateRows :'1.6fr 0.4fr'}}>
-                <div className='flex flex-wrap'>
-                    {cartitems?.map((data) => {
-                            return (
-                            <>
-                            <CartProductCard  carditems = {data}  key = {data.id} />
-                            </>
-                            )
-                        })}
-                </div>
+          <div style = {{display:'grid' , gridTemplateRows :'1.6fr 0.4fr'}}>
+                
+                  <div className='flex flex-wrap'>
+                      {cartitems?.map((data) => {
+                              return (
+                              <>
+                              <CartProductCard  carditems = {data}  key = {data.id} />
+                              </>
+                              )
+                          })}
+                  </div>
 
-        <div class="flex flex-col items-center pb-10">
-                <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src= '/F-RqNytXAAEFtrI.jpg' alt="Bonnie image"/>
-                <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white"> New item </h5>
-                <span class="text-sm text-gray-500 dark:text-gray-400">New Desc. </span>
-                <div class="flex mt-4 md:mt-6">
-                    <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">   Total Quantity = {TotalQuantity} </a>
-                    <a href="#" class="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"> TotalPrice =  Rs.{TotalPrice} </a>
-                </div>
-                    <button  onClick={handlepayment}> Pay Now </button>
+                  <div class="flex flex-col items-center pb-10">
+    
+                    <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        
+                        <div>
+                            <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white"> Price Details </h5>
+                        </div>
+
+                        <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+                        Total Quantity = {TotalQuantity} 
+                        </p>
+
+                        <a href="#" class="inline-flex font-medium items-center text-blue-600 hover:underline">
+                          TotalPrice =  Rs.{TotalPrice}
+                        </a>
+
+                        <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={handlepayment}> Checkout </button>
+                    </div>
+              
+                  </div>
+
           </div>
-        </div>
                     : 
             <>
             <h5> No  Products Available </h5>  
-            <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">  <NavLink to= '/shop'>Buy Products  </NavLink> </button>
+            <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">  <NavLink to= '/product'>Buy Products  </NavLink> </button>
             </>
     }
     </div>
--- frontend/src/components/Cart/CartProductCard.jsx
+++ frontend/src/components/Cart/CartProductCard.jsx
@@ -27,26 +27,36 @@ const CartProductCard = ({carditems}) => {
   return (
     <>
 
-        <a href="#" class="flex flex-row items-center bg-white border  border-black rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 my-6   dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <a href="#" class="flex flex-col items-center bg-white border  border-black rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 my-6   dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
            
          
-                <img class="object-cover w-full rounded-t-lg h-96  md:h-auto md:w-48  md:rounded-none md:rounded-s-lg" src={images[0]} alt={title} />
+                <img class="object-contain w-full rounded-t-lg h-52  md:h-auto md:w-48  md:rounded-none md:rounded-s-lg" src={images[0]} alt={title} />
                 
              <div>
                 <div class="flex flex-col justify-between p-4 leading-normal">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> {title}  </h5>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                    {description}
-                    </p>
-                    <p> Rs.={price}/- </p>
+                    <div>
+                        <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> {title}  </h3>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        {description}
+                        </p>
+                        <p className='text-2xl font-bold tracking-tight text-gray-900 '> Rs.={price}/- </p>
+                    </div>
 
-                    <button  onClick = {() => checkincrement(carditems)}   class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"> Inc ++
-                    </button>
-                        <span> {quantity} </span>
-                    <button   disabled = {quantity <= 1 }  onClick = {() => checkdecrement(carditems)}  class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"> Dec -- </button>
+                    <div className='flex flex-row py-4'>
+                        <button  onClick = {() => checkincrement(carditems)}   class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"> Inc ++
+                        </button>
+                        
+                        <span className='flex justify-center items-center text-black font-bold text-xl '> {quantity} </span>
 
-                    <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-                    onClick={() => RemovefromCart(id)}>  Remove from  Cart </button>
+                        <button   disabled = {quantity <= 1 }  onClick = {() => checkdecrement(carditems)}  class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"> Dec -- </button>
+                    </div>
+
+                    <div>
+
+                        <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+                        onClick={() => RemovefromCart(id)}>  Remove from  Cart </button>
+
+                    </div>
 
                 </div>
             </div>
